b661c99bcc322b5ecb40afc1032753fe74d26077
ThaisFReis <reis.thaisf@gmail.com>
2022-12-23 17:08:33 -0300

    DB atualizado

--- src/db.js
+++ src/db.js
@@ -3,11 +3,10 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const connection = new Pool({
-  host: 'localhost',
-  port: 5432,
-  user: 'postgres',
-  password: '123',
-  database: 'shortly'
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default connection;
